1fda909837967eefa66d23310ddb20b74bb5efa1
Jun Mei <cadnium@gmail.com>
2012-05-20 09:22:40 -0400

    initial add for problem 28
    Signed-off-by: Jun Mei <cadnium@gmail.com>

---
+++ p028/src/solution.js
@@ -0,0 +1,37 @@
+/**
+ * @license Copyright (c) 2012, Jun Mei
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+var ans = (function() {
+    var max = 1001;
+    var corners = [];
+
+    for ( var n = 3; n <= max; n += 2) {
+        corners.push(4 * n * n - 6 * n + 6);
+    }
+
+    var result = corners.reduce(function(sum, value) {
+        return sum + value;
+    }, 1);
+
+    return result;
+})();
+
+print(ans);
