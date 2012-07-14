bfff7c43b6a7aa25927e086d19ebdc84d20cbd65
Jun Mei <cadnium@gmail.com>
2012-07-14 12:19:16 -0400

    simplified tetration computation
    Signed-off-by: Jun Mei <cadnium@gmail.com>

--- p188/src/solution.py
+++ p188/src/solution.py
@@ -20,55 +20,11 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
-import math
-
-def sieve_primes(limit):
-    """
-    Returns all prime numbers less than or equal to the given upper bound; this
-    method implements the Sieve of Atkin for prime generation.
-    """
-    if limit < 2:
-        return []
-    (q, r) = divmod(limit, 2)
-    lng = q + r - 1
-    sieve = [True] * (lng + 1)
-    for i in range(int(math.sqrt(limit)) >> 1):
-        if not sieve[i]:
-            continue
-        inc = (i << 1) + 3
-        for j in range((i * (i + 3) << 1) + 3, lng, inc):
-            sieve[j] = False
-    result = [2]
-    result.extend([(i << 1) + 3 for i in range(lng) if sieve[i]])
-    return result
-
-def totient_chain(n):
-    primes = sieve_primes(n)
-    result = dict()
-    while n > 2:
-        totient = n
-        m = (n >> 1)
-        for x in primes:
-            if x > m:
-                break
-            if (n % x) == 0:
-                totient -= (totient // x)
-        result.setdefault(n, totient)
-        n = totient
-    result.setdefault(1, 1)
-    result.setdefault(2, 1)
-    return result
 
 def tetration(a, b, m):
-    totients = totient_chain(m)
-    hyper_exp = list()
-    while b > 0:
-        hyper_exp.append(m)
-        m, b = totients[m], b - 1
-    hyper_exp.reverse()
     result = a
-    for n in hyper_exp:
-        result = pow(a, result, n)
+    while b > 0:
+        result, b = pow(a, result, m), b - 1
     return result
 
 if __name__ == '__main__':
